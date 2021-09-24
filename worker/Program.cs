aa0a339f1d0aa9dea5df8edaa9300667796fea45
Thorsten Hans <thorsten.hans@gmail.com>
2021-09-24 21:37:21 +0200

    chore: add worker
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

---
+++ worker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace BASTA.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Doing Work ....");
+            Thread.Sleep(10*1000);
+            Console.WriteLine("Work Done!");
+        }
+    }
+}
